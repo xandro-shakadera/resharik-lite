07edce5e6f9e83f81bb3b124a273f830b17dcf23
xandro-shakadera <astex_corp@outlook.com>
2025-03-01 17:40:10 +0300

    First commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('title', 'Главная')
+@section('content')
+    <div class='container-fluid text-center my-5'>
+        <img class="w-25 h-25" src="images/turtle.png" alt="">
+        <h1>Решарик Лайт</h1>
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title') - Решарик</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="{{ route('index') }}">Решарик</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{-- route('subjects') --}}">Предметы</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{-- route('maths') --}}">Решение примеров</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{-- route('blogs') --}}">Полезные блоги</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    @yield('content')
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
